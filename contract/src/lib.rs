88f80c481cc2ef2031903240ab201f431c8b895e
martyr <leonyvb04@gmail.com>
2022-02-20 17:00:02 +0200

    remove comments

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -10,11 +10,10 @@
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
+use near_sdk::serde::de::Unexpected::Str;
 
 near_sdk::setup_alloc!();
 
-// add the following attributes to prepare your code for serialization and invocation on the blockchain
-// More built-in Rust attributes here: https://doc.rust-lang.org/reference/attributes.html#built-in-attributes-index
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Counter {
@@ -37,6 +36,11 @@ impl Counter {
     /// ```
     pub fn get_num(&self) -> i8 {
         return self.val;
+    }
+
+    pub fn get_text(&self) -> &'static str {
+        let text = "Hello, world";
+        return text;
     }
 
     /// Increment the counter.
@@ -102,62 +106,3 @@ fn after_counter_change() {
  */
 
 // use the attribute below for unit tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use near_sdk::MockedBlockchain;
-    use near_sdk::testing_env;
-    use near_sdk::test_utils::VMContextBuilder;
-    use near_sdk::json_types::ValidAccountId;
-    use near_sdk::serde::export::TryFrom;
-
-    // simple helper function to take a string literal and return a ValidAccountId
-    fn to_valid_account(account: &str) -> ValidAccountId {
-        ValidAccountId::try_from(account.to_string()).expect("Invalid account")
-    }
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: ValidAccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
-    }
-
-    // mark individual unit tests with #[test] for them to be registered and fired
-    #[test]
-    fn increment() {
-        // set up the mock context into the testing environment
-        let context = get_context(to_valid_account("foo.near"));
-        testing_env!(context.build());
-        // instantiate a contract variable with the counter at zero
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        println!("Value after increment: {}", contract.get_num());
-        // confirm that we received 1 when calling get_num
-        assert_eq!(1, contract.get_num());
-    }
-
-    #[test]
-    fn decrement() {
-        let context = VMContextBuilder::new();
-        testing_env!(context.build());
-        let mut contract = Counter { val: 0 };
-        contract.decrement();
-        println!("Value after decrement: {}", contract.get_num());
-        // confirm that we received -1 when calling get_num
-        assert_eq!(-1, contract.get_num());
-    }
-
-    #[test]
-    fn increment_and_reset() {
-        let context = VMContextBuilder::new();
-        testing_env!(context.build());
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        contract.reset();
-        println!("Value after reset: {}", contract.get_num());
-        // confirm that we received -1 when calling get_num
-        assert_eq!(0, contract.get_num());
-    }
-}
